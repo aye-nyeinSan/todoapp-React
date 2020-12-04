0f767207e34e57ccfa2076c022b7c888d38ab187
aye-nyeinSan <56792505+aye-nyeinSan@users.noreply.github.com>
2020-12-04 21:32:55 +0630

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,14 +8,17 @@ class App extends Component{
         super(props);
 
          this.state={
-                text:'',items:[]
+                text:'',items:[],
          }
+         this.Reset=this.Reset.bind(this);
          this.handlesubmit=this.handlesubmit.bind(this);
          this.handlechange=this.handlechange.bind(this);
     }
+
     handlechange(e){
         this.setState( {text: e.target.value});
     }
+
  handlesubmit (e ){
     e.preventDefault();
     if(this.state.text.length === 0) return ;
@@ -23,15 +26,24 @@ class App extends Component{
            text: this.state.text,
            id:Date.now()
      };
-        this.setState(state=>({items:state.items.concat(newItem),text:'' }));
- }
-   
+        this.setState(state=>({items:state.items.concat(newItem),text:'' }));}
+           
+        
+        Reset(){
+                this.setState({items:[],text:''})
+            }
+         
+       
+
      
     render(){
         return(
         <div>
             <h1> ToDo_List </h1>
-            <Todolist items={this.state.items}/> 
+
+            <Todolist items={this.state.items} 
+            reset={this.Reset} /> 
+
            <form onSubmit={this.handlesubmit}>
            <label>
                 What you have to do Today!!
@@ -43,6 +55,10 @@ class App extends Component{
             <br></br>
 
             <button >Add {this.state.items.length+1} </button>
+
+              <button onClick={this.Reset} >
+               Finish
+                 </button>
            </form>
            
 
--- src/Todolist.js
+++ src/Todolist.js
@@ -1,21 +1,23 @@
 import React ,{Component } from 'react';
 
 class Todolist extends Component{
-    constructor(props)
-    {super(props)};
+   
     
 render(){
+    
+  
     return(
         <div>
-          
-          
+         
         {this.props.items.map(item => (
           <ul><label><input type="checkbox"/> {item.text}</label>
           <br></br></ul>
           
         ))}
-    
+
+        
         </div>
+
     )
 }
 } 
